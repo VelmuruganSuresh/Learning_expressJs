bdbb55c467ebc9c94b789b12eff7babaa30ebb79
VelmuruganSuresh <velmurugan812415@gmail.com>
2025-09-15 18:37:25 +0530

    committed

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('hello');
+    
+})
+
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log('server is running...');
+    
+})
